f6ebc5a19d95043da87d9dfd6a44236db8cf95eb
Illyrian <ilirprevazi@gmail.com>
2022-10-05 15:00:25 +0200

    enter and space button

--- scripts.js
+++ scripts.js
@@ -17,10 +17,7 @@ const volumeP = document.getElementById("volume-p");
 const massP = document.getElementById("mass-p");
 
 
-
-
-inputBtn.addEventListener("click", function(){
-
+function test(){
     let insertedValue = inputBox.value;
 
     let ctFeet = insertedValue * 3.28084;
@@ -30,15 +27,36 @@ inputBtn.addEventListener("click", function(){
     let ctGallons = insertedValue / 0.264172;
 
     let ctKilograms = insertedValue * 2.20462;
-    let ctPounds = insertedValue / 2.20462;
+    let ctPounds = insertedValue / 2.204;
 
 
     lengthP.innerHTML = `${insertedValue} meters = ${ctFeet.toFixed(3)} feet | ${insertedValue} feet = ${ctMeters.toFixed(3)} meters`;
     volumeP.innerHTML = `${insertedValue} liters = ${ctLiters.toFixed(3)} gallons | ${insertedValue} gallons = ${ctGallons.toFixed(3)} liters`;
     massP.innerHTML = `${insertedValue} kilos = ${ctKilograms.toFixed(3)} pounds | ${insertedValue} pounds = ${ctPounds.toFixed(3)} kilos`;
+}
+
+function clear(){
+    inputBox.value = " ";
+    lengthP.innerHTML = ``;
+    volumeP.innerHTML = ``;
+    massP.innerHTML = ``;
+}
+
+
+inputBtn.addEventListener("click", function(){
+
+    test();
 
 });
 
 
 
+
+inputBox.addEventListener("keypress", function (e) {
+    if (e.key === 'Enter') {
+        test();
+    } else if (e.key === ' '){
+        clear();
+    }
+});
 

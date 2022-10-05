ffca0d7da13ed5d7e33c1b4772fa25387cd13479
Illyrian <ilirprevazi@gmail.com>
2022-10-05 10:50:30 +0200

    first try without js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// //pjesa e inputit
+
+// const inputBox = document.getElementById("input-box");
+// const inputBtn = document.getElementById("input-btn");
+
+// inputBox.addEventListener("click", function(){
+//     inputBox.placeholder = " ";
+// })
